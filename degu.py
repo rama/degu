4f3f4198ea8d6c3528da196eaf2583c814aae450
Ramapriya Radhakrishnan <67516691+rama@users.noreply.github.com>
2025-01-21 01:25:23 -0500

    parse html and construct tree

--- degu.py
+++ degu.py
@@ -4,7 +4,7 @@ import ssl
 
 class URL:
     def __init__(self, url):
-        if url[-1] != "/":
+        if not url.endswith(".html") and url[-1] != "/":
             url += "/"
         self.scheme, url = url.split("://", 1)
         if self.scheme not in ["http", "https"]:
@@ -53,31 +53,85 @@ class URL:
         s.close()
         return content
 
-    def show(self, body):
+
+class Text:
+    def __init__(self, text, parent):
+        self.text = text
+        self.children = []
+        self.parent = parent
+
+    def __repr__(self):
+        return repr(self.text)
+
+
+class Element:
+    def __init__(self, tag, parent):
+        self.tag = tag
+        self.children = []
+        self.parent = parent
+
+    def __repr__(self):
+        return "<" + self.tag + ">"
+
+
+class HTMLParser:
+    def __init__(self, body):
+        self.body = body
+        self.unfinished = []
+
+    def add_text(self, text):
+        parent = self.unfinished[-1]
+        node = Text(text, parent)
+        parent.children.append(node)
+
+    def add_tag(self, tag):
+        if tag.startswith("/"):
+            if len(self.unfinished) == 1:
+                return
+            node = self.unfinished.pop()
+            parent = self.unfinished[-1]
+            parent.children.append(node)
+        else:
+            parent = self.unfinished[-1] if self.unfinished else None
+            node = Element(tag, parent)
+            self.unfinished.append(node)
+
+    def finish(self):
+        while len(self.unfinished) > 1:
+            node = self.unfinished.pop()
+            parent = self.unfinished[-1]
+            parent.children.append(node)
+        return self.unfinished.pop()
+
+    def parse(self):
+        text = ""
         in_tag = False
-        entities = {"lt": "<", "gt": ">,"}
-        is_entity = False
-        current_entity = ""
-        for c in body:
+        for c in self.body:
             if c == "<":
                 in_tag = True
+                if text:
+                    self.add_text(text)
+                text = ""
             elif c == ">":
                 in_tag = False
-            elif c == "&":
-                is_entity = True
-            elif is_entity and c == ";":
-                print("{current_entity=}")
-                current_entity = ""
-                is_entity = False
-            elif is_entity:
-                current_entity += c
-            elif not in_tag and not is_entity:
-                print(c, end="")
+                self.add_tag(text)
+                text = ""
+            else:
+                text += c
+        if not in_tag and text:
+            self.add_text(text)
+        return self.finish()
+
+
+def print_tree(node, indent=0):
+    print(" " * indent, node)
+    for child in node.children:
+        print_tree(child, indent + 2)
 
 
 def load(url):
-    body = url.request()
-    url.show(body)
+    tree = HTMLParser(url.request()).parse()
+    print_tree(tree)
 
 
 if __name__ == "__main__":
